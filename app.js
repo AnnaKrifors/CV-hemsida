da61f6afb7db090d7f47d94c0b0d5bb481590c02
AnnaKrifors <113009629+AnnaKrifors@users.noreply.github.com>
2023-01-09 13:47:11 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const phoneLi = document.querySelector('ul li');
+const mailLi = document.querySelector('ul li :nth-child(2)');
+const adressLi = document.querySelector('ul li :nth-child(3)');
+const phone = document.querySelector('#phone');
+const mail = document.querySelector('#mail');
+
+
+
+
+//fixa flexbox så det ligger rätt
+//fixa så vid klick av ikon öppnas mobilring och mail
+//fixa så texten ser snygg ut i tabellen
+//
